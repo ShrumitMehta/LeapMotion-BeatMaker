921522759b7e1c5f95d21be4b64f7ac275d9e2a2
tomkay94 <thanasi.karachotzitis@mail.utoronto.ca>
2015-03-29 01:03:52 -0800

    Remove song samples.

--- GestureController.java
+++ GestureController.java
@@ -2,6 +2,10 @@ package main;
 
 import com.leapmotion.leap.*;
 
+/*
+ * Handles gesture signals coming from the Leap Motion.
+ * */
+
 public class GestureController extends Listener {
 
 	/* Configure the accepted gesture types. */
@@ -28,11 +32,19 @@ public class GestureController extends Listener {
 			switch (gesture.type()) {
 				case TYPE_SWIPE:
 					System.out.println("SWIPE");
-					new SoundPlayer("34_Get_Treasure_Box.wav").start();
+					new SoundPlayer("triple.wav").start();
+					break;
+				case TYPE_CIRCLE:
+					System.out.println("CIRCLE");
+					new SoundPlayer("melodic.wav").start();					
 					break;
 				case TYPE_KEY_TAP:
-					System.out.println("TAP");
-					new SoundPlayer("beatwav.wav").start();
+					System.out.println("KEY TAP");
+					new SoundPlayer("10sec.wav").start();
+					break;
+				case TYPE_SCREEN_TAP:
+					System.out.println("SCREEN TAP");
+					new SoundPlayer("scratch.wav").start();
 					break;
 				default:
 					System.out.println(gesture.type());
@@ -40,34 +52,39 @@ public class GestureController extends Listener {
 			}
 		}
 	}
-	
+
 	/* Map action to a sound */
 	private void determineActionFromFrame(Frame frame) {
-		
+
 		for (Hand h : frame.hands()) {
-			
+
 			/* Only run this if the last action has been delayed */
-				if (h.isValid()) {
-					/* Left hand fist */
-					if (h.isLeft() && h.grabStrength() == 1.0) {
-						System.out.println("Left hand fist" + ": " + h.grabStrength());
-					}
-					
-					/* Left hand not fist */
-					else if (h.isLeft() && h.grabStrength() == 0.0) {
-						System.out.println("Left hand not fist" + ": " + h.grabStrength());		
-					}
-					
-					/* Right hand fist */
-					else if (h.isRight() && h.grabStrength() == 1.0) {
-						System.out.println("Right hand fist" + ": " + h.grabStrength());
-					}
-					
-					/* Right hand not fist */
-					else if (h.isRight() && h.grabStrength() == 0.0) {
-						System.out.println("Right hand not fist" + ": " + h.grabStrength());				
-					}				
+			if (h.isValid()) {
+				/* Left hand fist */
+				if (h.isLeft() && h.grabStrength() == 1.0) {
+					System.out.println("Left hand fist" + ": "
+							+ h.grabStrength());
+				}
+
+				/* Left hand not fist */
+				else if (h.isLeft() && h.grabStrength() == 0.0) {
+					System.out.println("Left hand not fist" + ": "
+							+ h.grabStrength());
+				}
+
+				/* Right hand fist */
+				else if (h.isRight() && h.grabStrength() == 1.0) {
+					System.out.println("Right hand fist" + ": "
+							+ h.grabStrength());
+//					System.exit(0);
+				}
+
+				/* Right hand not fist */
+				else if (h.isRight() && h.grabStrength() == 0.0) {
+					System.out.println("Right hand not fist" + ": "
+							+ h.grabStrength());
 				}
 			}
 		}
+	}
 }
