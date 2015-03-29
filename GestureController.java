baa8afb07114b3eb14b37ec21258cbef699fc783
Evan Finnigan <efinnig@uwo.ca>
2015-03-29 09:08:12 -0400

    Commit :monkey_face:
    :saxophone:

--- GestureController.java
+++ GestureController.java
@@ -1,48 +1,146 @@
 package main;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Panel;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.nio.file.Files;
 
 import javax.sound.sampled.SourceDataLine;
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.event.ListSelectionListener;
 
 import com.leapmotion.leap.*;
 
-
 /*
-	A controller which handles built-in 
-	hand gestures from the LeapMotion.
-*/
+ A controller which handles built-in 
+ hand gestures from the LeapMotion.
+ */
 
 public class GestureController extends Listener {
 
-	private Controller controller;
 	private JFrame jframe = new JFrame();
-	private JPanel panel = new JPanel();
-	private JLabel label = new JLabel();
+	private boolean isLooping;
+	private boolean paused;
+	private JButton loop;
+	private JButton pause;
+	private SoundPlayer player2;
+	private int selected;
+	private String[] files;
+	private JScrollPane fileList;
+	private JList<String> list;
+
+	public GestureController() {
+		this.isLooping = false;
+		this.paused = false;
+		this.selected = 0;
+		String[] array = new String[4];
+		array[0] = "beatwav.wav";
+		array[1] = "guitar.wav";
+		array[2] = "chord.wav";
+		array[3] = "wobble.wav";
+		this.files = array;
+		this.list = new JList<String>(array);
+		setUpGui();
+	}
+
+	private void setUpGui() {
+		JPanel titlePanel = new JPanel();
+		titlePanel.setSize(jframe.getWidth(), 30);
+		titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
+		titlePanel.setBackground(new Color(255, 255, 255));
+
+		JLabel label = new JLabel("LEAP");
+		label.setFont(new Font("lrg", Font.PLAIN, 30));
+		label.setForeground(new Color(0, 0, 0));
+		titlePanel.add(label);
+		label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+		JLabel labelm = new JLabel("MOTION");
+		labelm.setFont(new Font("sml", Font.PLAIN, 12));
+		labelm.setForeground(new Color(93, 170, 0));
+		titlePanel.add(labelm);
+		labelm.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+		JPanel pane = new JPanel();
+		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+		pane.setBackground(new Color(244, 244, 244));
+
+		JLabel labelb = new JLabel("BeatMaker");
+		labelb.setFont(new Font("sml", Font.PLAIN, 50));
+		labelb.setForeground(new Color(238, 24, 24));
+		pane.add(labelb);
+		labelb.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+		loop = new JButton("Loop Sounds");
+		pane.add(loop);
+		loop.setAlignmentX(JButton.CENTER_ALIGNMENT);
+
+		loop.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if (isLooping) {
+					loop.setText("Loop sounds");
+				} else {
+					loop.setText("Play Once");
+				}
+				isLooping = !isLooping;
+			}
+		});
+		
+		pause = new JButton("Pause Loops");
+		pane.add(pause);
+		pause.setAlignmentX(JButton.CENTER_ALIGNMENT);
+
+		pause.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				if (paused) {
+					loop.setText("Play Loops");
+					player2.setPause(true);
+				} else {
+					loop.setText("Pause Loops");
+					player2.setPause(false);
+				}
+				paused = !paused;
+			}
+		});
+		
+		fileList = new JScrollPane(list);
+		pane.add(fileList);
+		fileList.setAlignmentX(JScrollPane.CENTER_ALIGNMENT);
+		list.setSelectedIndex(selected);
+		
+		jframe.setLayout(new BorderLayout());
+		jframe.add(titlePanel, BorderLayout.NORTH);
+		jframe.add(pane, BorderLayout.CENTER);
+		jframe.setBackground(new Color(17, 17, 17));
+		jframe.setVisible(true);
+		jframe.setTitle("LeapMotion BEATMAKER - Version 0.1");
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();
+		double height = screenSize.getHeight();
+		jframe.setBounds((int) (width * 0.1), (int) (height * 0.1),
+				(int) (width * 0.8), (int) (height * 0.8));
+		jframe.pack();
+		jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
 
 	/* Configure the accepted gesture types. */
 	@Override
 	public void onConnect(Controller controller) {
-		this.controller = controller;
 		System.out.println("Application connected");
 		controller.enableGesture(Gesture.Type.TYPE_SWIPE);
 		controller.enableGesture(Gesture.Type.TYPE_CIRCLE);
 		controller.enableGesture(Gesture.Type.TYPE_KEY_TAP);
 		controller.enableGesture(Gesture.Type.TYPE_SCREEN_TAP);
-		label = new JLabel("LeapMotion-BeatMaker");
-		label.setFont(new Font("lrg", Font.BOLD, 30));
-		label.setForeground(Color.BLUE);
-		label.setBackground(Color.BLACK);
-		panel = new JPanel();
-		panel.add(label);
-		panel.setBackground(Color.BLACK);
-		jframe.add(panel);
-		jframe.setBackground(Color.BLACK);
-		jframe.setVisible(true);
-		jframe.setSize(new Dimension(1600,900));
-		jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
 	/* Act on a captured frame */
@@ -50,69 +148,31 @@ public class GestureController extends Listener {
 	public void onFrame(Controller controller) {
 		Frame frame = controller.frame();
 		determineGestureFromFrame(frame);
-		determineActionFromFrame(frame);
 	}
 
 	/* Map action to a sound */
 	private void determineGestureFromFrame(Frame frame) {
 		for (Gesture gesture : frame.gestures()) {
 			switch (gesture.type()) {
-				case TYPE_SWIPE:
-					System.out.println("SWIPE");
-					new SoundPlayer("triple.wav").start();
-					break;
-				case TYPE_CIRCLE:
-					System.out.println("CIRCLE");
-					new SoundPlayer("melodic.wav").start();					
-					break;
-				case TYPE_KEY_TAP:
-					System.out.println("KEY TAP");
-					new SoundPlayer("10sec.wav").start();
-					break;
-				case TYPE_SCREEN_TAP:
-					System.out.println("SCREEN TAP");
-					new SoundPlayer("scratch.wav").start();
-					break;
-				default:
-					System.out.println(gesture.type());
-					break;
+			case TYPE_SCREEN_TAP:
+				System.out.println("Screen Tap");
+				selected = selected + 1;
+				if (selected >= files.length){
+					selected = 0;
+				}
+				list.setSelectedIndex(selected);
+				break;
+			case TYPE_KEY_TAP:
+				System.out.println("Tap");
+				player2 = new SoundPlayer(files[selected]);
+				player2.setLooping(isLooping);
+				player2.start();
+				break;
+			default:
+				System.out.println(gesture.type());
+				break;
 			}
 		}
 	}
-	
-	/* Map action to a sound */
-	private void determineActionFromFrame(Frame frame) {
 
-		for (Hand h : frame.hands()) {
-
-			/* Only run this if the last action has been delayed */
-			if (h.isValid()) {
-				/* Left hand fist */
-				if (h.isLeft() && h.grabStrength() == 1.0) {
-					System.out.println("Left hand fist" + ": "
-							+ h.grabStrength());
-				}
-
-				/* Left hand not fist */
-				else if (h.isLeft() && h.grabStrength() == 0.0) {
-					System.out.println("Left hand not fist" + ": "
-							+ h.grabStrength());
-				}
-
-				/* Right hand fist */
-				else if (h.isRight() && h.grabStrength() == 1.0) {
-					System.out.println("Right hand fist" + ": "
-							+ h.grabStrength());
-//					System.exit(0);
-				}
-
-				/* Right hand not fist */
-				else if (h.isRight() && h.grabStrength() == 0.0) {
-					System.out.println("Right hand not fist" + ": "
-							+ h.grabStrength());
-				}
-			}
-		}
-	}
-	
 }
--- SoundPlayer.java
+++ SoundPlayer.java
@@ -1,4 +1,5 @@
 package main;
+
 import java.io.File;
 import java.io.IOException;
 import javax.swing.*;
@@ -11,85 +12,102 @@ import javax.sound.sampled.SourceDataLine;
 
 public class SoundPlayer extends Thread {
 
-    private final int BUFFER_SIZE = 128000;
-    private File soundFile;
-    private AudioInputStream audioStream;
-    private AudioFormat audioFormat;
-    private SourceDataLine sourceLine;
-    private String strFileName;
+	private final int BUFFER_SIZE = 128000;
+	private File soundFile;
+	private AudioInputStream audioStream;
+	private AudioFormat audioFormat;
+	private SourceDataLine sourceLine;
+	private String strFileName;
+	private boolean looping;
+	private boolean pause;
 
-    public SoundPlayer(String name) {
-    	this.strFileName = name;
-    }
+	public SoundPlayer(String name) {
+		this.strFileName = name;
+	}
 
-    public String getStrFileName() {
-    	return strFileName;
-    }
+	public String getStrFileName() {
+		return strFileName;
+	}
 
-    public SourceDataLine getDataLine(){
-    	return sourceLine;
-    }
-    
-    public void run(){
-    	
-    	for (int i = 0; i < 1; i++){
-    		if (sourceLine != null && sourceLine.isActive()) {
-        		
-        	} else {
-        		play();
-        	}
-    	}
-    }
-    
-    /**
-     * @param filename the name of the file that is going to be played
-     */
-    public void play(){
-    	
-        try {
-            soundFile = new File(getStrFileName());
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+	public SourceDataLine getDataLine() {
+		return sourceLine;
+	}
 
-        try {
-            audioStream = AudioSystem.getAudioInputStream(soundFile);
-        } catch (Exception e){
-            e.printStackTrace();
-            System.exit(1);
-        }
+	public void setLooping(boolean b) {
+		looping = b;
+	}
+	
+	public void setPause(boolean b){
+		pause = b;
+	}
 
-        audioFormat = audioStream.getFormat();
+	public void run() {
 
-        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-        try {
-            sourceLine = (SourceDataLine) AudioSystem.getLine(info);
-            sourceLine.open(audioFormat);
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-            System.exit(1);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+		if (looping) {
+			// for (int i = 0; i < 10; i++){
+			while (looping) {
+				if (sourceLine != null && sourceLine.isActive()) {
 
-        int nBytesRead = 0;
-        byte[] abData = new byte[BUFFER_SIZE];
-        while (nBytesRead != -1) {
-            try {
-                nBytesRead = audioStream.read(abData, 0, abData.length);
-                sourceLine.start();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (nBytesRead >= 0) {
-                @SuppressWarnings("unused")
-                int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
-            }
-        }
+				} else {
+					play();
+				}
+			}
+		} else {
+			play();
+		}
+	}
 
-        sourceLine.drain();
-        sourceLine.close();
-    }
+	/**
+	 * @param filename
+	 *            the name of the file that is going to be played
+	 */
+	public void play() {
+
+		try {
+			soundFile = new File(getStrFileName());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		try {
+			audioStream = AudioSystem.getAudioInputStream(soundFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		audioFormat = audioStream.getFormat();
+
+		DataLine.Info info = new DataLine.Info(SourceDataLine.class,
+				audioFormat);
+		try {
+			sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+			sourceLine.open(audioFormat);
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+			System.exit(1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		int nBytesRead = 0;
+		byte[] abData = new byte[BUFFER_SIZE];
+		while (nBytesRead != -1) {
+			try {
+				nBytesRead = audioStream.read(abData, 0, abData.length);
+				sourceLine.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (nBytesRead >= 0) {
+				@SuppressWarnings("unused")
+				int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
+			}
+		}
+
+		sourceLine.drain();
+		sourceLine.close();
+	}
 }
